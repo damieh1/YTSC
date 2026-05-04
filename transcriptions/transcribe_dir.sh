70a6921f985e75eed95e4232f57b458bf2ed0f39
damieh <81173039+damieh1@users.noreply.github.com>
2026-05-04 11:10:18 -0400

    Update copyright notice in transcribe_dir.sh

--- transcriptions/transcribe_dir.sh
+++ transcriptions/transcribe_dir.sh
@@ -1,7 +1,7 @@
 # transcribe_dir.sh - batch transcription wrapper for Whisper
 
 # Code below is adapted from community examples of Whisper CLI usage.
-# Copyright (c) 2025 XXXX.XXXX
+# Copyright (c) 2025 damieh1
 # Licensed under the MIT License (see LICENSE file for details).
 
 ---
